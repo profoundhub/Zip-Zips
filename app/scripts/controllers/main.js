c182b7dd7636fcfa779b51d5fdb1ed49fe8632b6
Daniel Lim <hbt@markhamboards.com>
2016-09-25 17:16:13 -0400

    zipzip #1
    zipzip #1

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zipsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zipsApp
+ */
+angular.module('ziplineFcc2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
